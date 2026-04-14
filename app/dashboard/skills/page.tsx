a1bb7cd92d54af8d7c389fb6a61b5e89b2fabeb1
filip patrman <filip.patrman.023@pslib.cz>
2026-04-14 14:22:58 +0200

    refactor: cast skills prop to SkillLibrary type in SkillsPage

--- app/dashboard/skills/page.tsx
+++ app/dashboard/skills/page.tsx
@@ -1,11 +1,12 @@
 
 import { getSkills } from "@/services/skills.service";
 import SkillsClient from "./SkillsClient";
+import { SkillLibrary } from "@/types/training";
 
 
 export default async function SkillsPage() {
     const skills = await getSkills();
     if (!skills) return [];
     
-    return <SkillsClient skills={skills}/>
+    return <SkillsClient skills={skills as SkillLibrary[]}/>
 }
